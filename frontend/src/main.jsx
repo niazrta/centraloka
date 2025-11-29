b84646c6792465f20f044f24b08d717ed3e102ad
niazrta <insaniamalia34@gmail.com>
2025-11-29 19:51:00 +0700

    pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,20 @@ import './index.css'
 // Import Provider yang sudah dibuat
 import { AuthProvider } from './context/AuthContext'
 
+import { registerSW } from 'virtual:pwa-register'
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('Konten baru tersedia. Refresh sekarang?')) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    console.log('Aplikasi siap bekerja offline!')
+  },
+})
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
